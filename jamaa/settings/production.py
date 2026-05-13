93e07522e4a45d9f7a8d793bba6272258664b99e
pheebemi <lemuelemmanuel29@gmail.com>
2026-05-13 15:12:25 +0100

    ssf

--- jamaa/settings/production.py
+++ jamaa/settings/production.py
@@ -1,16 +1,17 @@
 from .base import *
-import dj_database_url
 from decouple import config
 
 DEBUG = False
 
 DATABASES = {
-    'default': dj_database_url.parse(config('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_HSTS_SECONDS = 31536000
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
+CORS_ALLOW_ALL_ORIGINS = True
+
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
